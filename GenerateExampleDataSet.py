dbb051e99f0559ac24101385f2b11e37df3bd9b6
Marty <zwitter689@gmail.com>
2015-11-12 21:18:41 -0500

    Csthing

--- GenerateExampleDataSet.py
+++ GenerateExampleDataSet.py
@@ -7,7 +7,7 @@ D = 2 # dimensionality
 K = 3 # number of classes
 X = np.zeros((N*K,D)) # data matrix (each row = single example)
 y = np.zeros(N*K, dtype='uint8') # class labels
-for j in xrange(K):
+for j in range(K):
   ix = range(N*j,N*(j+1))
   r = np.linspace(0.0,1,N) # radius
   t = np.linspace(j*4,(j+1)*4,N) + np.random.randn(N)*0.2 # theta
